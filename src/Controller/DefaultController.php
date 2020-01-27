39d99b7610e89ed55a5c2f4ab098a8787ba09ac5
Sergey HTML <i@b9sk.ru>
2020-01-27 05:17:01 +0300

    hide index page for all logged in users redirecting them to dashboard

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,12 +10,20 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends AbstractController
 {
-    function index()
+    function index(UrlGeneratorInterface $urlGenerator)
     {
-        return $this->render('default/index.html.twig');
+        // if user is logged in then redirect to dashboard
+        //   - else return render of static index.html
+        if ( $this->getUser() && array_search("ROLE_USER", $this->getUser()->getRoles()) !== false ) {
+            return new RedirectResponse($urlGenerator->generate('dashboard'));
+        }
+        else {
+            return $this->render('default/index.html.twig');
+        }
     }
-    
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -73,8 +73,11 @@ class User implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        
+        // guarantees that a user always has at least one role for security
+        if (empty($roles)) {
+            $roles[] = 'ROLE_USER';
+        }
 
         return array_unique($roles);
     }
