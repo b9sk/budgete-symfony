fc4e2b245d5f2264eb0eab19575b0a74b2d3d68d
Sergey HTML <i@b9sk.ru>
2020-01-27 05:15:02 +0300

    init dashboard controller

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -94,8 +94,8 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        // @todo: For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // if log in has been successful redirect the user to dashboard
+        return new RedirectResponse($this->urlGenerator->generate('dashboard'));
     }
 
     protected function getLoginUrl()
