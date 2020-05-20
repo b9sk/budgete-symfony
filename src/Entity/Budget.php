d24519f126cf20f416477fc722ab6c6d08abfbdc
Sergey <narkogoleg@gmail.com>
2020-05-20 18:23:08 +0300

    a max number constraint

--- src/Entity/Budget.php
+++ src/Entity/Budget.php
@@ -3,7 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BudgetRepository")
  */
@@ -24,6 +24,7 @@ class Budget
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\LessThan(2147483648)
      */
     private $amount;
 
